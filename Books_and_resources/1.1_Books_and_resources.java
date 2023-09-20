5cd39e5d99c04108b7ce039c12b888f633d28cb0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-20 15:12:45 +0300

    Create 1.1_Books_and_resources.java

---
+++ Books_and_resources/1.1_Books_and_resources.java
@@ -0,0 +1,28 @@
+To learn more about molecular biology, genetics, and the CRISPR-Cas9 system, you can explore various resources such as books, websites, and videos. Here are some recommendations:
+
+Books:
+1. "Molecular Biology of the Cell" by Bruce Alberts et al. - This comprehensive textbook provides a thorough introduction to molecular biology and covers various topics related to genetics and cellular processes.
+2. "Genetics: Analysis and Principles" by Robert J. Brooker - This textbook offers a comprehensive overview of genetics, including molecular genetics and genetic engineering.
+
+Websites:
+1. [National Human Genome Research Institute](https://www.genome.gov/) - This website provides information and resources on genomics research, including molecular biology and genetics.
+2. [Khan Academy](https://www.khanacademy.org/) - Khan Academy offers free online courses and tutorials on various topics, including molecular genetics and biology.
+3. [EdX](https://www.edx.org/) - EdX offers online courses on molecular biology and related subjects. You can search for specific courses that cover the topics you are interested in.
+
+Videos:
+1. [CRISPR-Cas9 and Targeted Genome Editing: New Era in Molecular Biology](https://www.youtube.com/watch?v=2pp17E4E-O8) - This video provides an introduction to the CRISPR-Cas9 system and its applications in genetic engineering.
+2. [Molecular Biology Crash Course](https://www.youtube.com/watch?v=7KkXkU1cv8g) - This video from Crash Course provides a concise overview of molecular biology concepts, including genetics and DNA replication.
+
+These resources should provide you with a solid foundation in molecular biology, genetics, and the CRISPR-Cas9 system. Remember to explore multiple sources and engage in hands-on learning experiences to deepen your understanding of these topics.
+
+
+
+
+
+
+
+
+
+
+
+  
